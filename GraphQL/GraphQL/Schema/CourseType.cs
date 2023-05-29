4053a8bec714110a808493c8c9c17148ab261915
bukhuti-vardanidze <84092360+bukhuti-vardanidze@users.noreply.github.com>
2023-05-29 13:59:30 +0400

    start GraphQL

---
+++ GraphQL/GraphQL/Schema/CourseType.cs
@@ -0,0 +1,18 @@
+﻿namespace GraphQL.Schema
+{
+    public enum Subject
+    {
+        Mathematics,
+        Science,
+        History
+    }
+
+    public class CourseType
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Subject Subject { get; set; }
+        public InstructorType Instructor { get; set; }
+        public IEnumerable<StudentType> Students { get; set; }
+    }
+}
---
+++ GraphQL/GraphQL/Schema/Query.cs
@@ -0,0 +1,32 @@
+﻿using Bogus;
+
+namespace GraphQL.Schema
+{
+    public class Query
+    {
+
+        public IEnumerable<CourseType> GetCourses()
+        {
+            Faker<CourseType> courseFaker = new Faker<CourseType>()
+                .RuleFor(c=>c.Id, f=> Guid.NewGuid())
+                .RuleFor(c => c.Name, f=> f.Name.JobTitle());
+
+            List<CourseType> courses = courseFaker.Generate(5);
+            return courses;
+
+            //return new List<CourseType>()
+            //{
+            //    Id = Guid.NewGuid(),
+            //    Name = "Geometry",
+            //    Subject = Subject.Mathematics,
+            //    Instructor = new InstructorType()
+            //    {
+            //        Id = Guid.NewGuid(),
+            //    }
+            //}
+        }
+
+        [GraphQLDeprecated("this query is depraceted")]
+        public string Instructions => "Hello GraphQl!";
+    }
+}
